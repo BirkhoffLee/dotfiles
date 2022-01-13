1a1ccc1fd77cc12bd79e15cb9be99a8abfac179b
Birkhoff Lee <git@birkhoff.me>
2022-01-13 17:59:57 +0800

    chore: update brewfile with --describe --force
    Signed-off-by: Birkhoff Lee <git@birkhoff.me>

--- update_brewfile.sh
+++ update_brewfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-rm Brewfile Brewfile.lock.json
-brew bundle dump
+# rm Brewfile Brewfile.lock.json
+brew bundle dump --describe --force
 git add Brewfile
 git commit -S -m "chore: update Brewfile"
