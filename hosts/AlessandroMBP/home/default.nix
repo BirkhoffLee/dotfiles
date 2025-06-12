d1378ee80472cb87d1426b062595099cc4ad4531
birkhoff <git@birkhoff.me>
2025-06-12 14:24:46 +0200

    docs(fzf): clarify key binding usage
    Added a comment to specify that C-r is used for atuin history search
    
    Signed-off-by: birkhoff <git@birkhoff.me>

--- hosts/AlessandroMBP/home/default.nix
+++ hosts/AlessandroMBP/home/default.nix
@@ -67,6 +67,7 @@ rec {
     fzf = {
       enable = true;
       # Use C-r and C-t from fzf
+      # Note: C-r is later used for atuin history search
       # https://github.com/junegunn/fzf?tab=readme-ov-file#key-bindings-for-command-line
       enableZshIntegration = true;
     };
