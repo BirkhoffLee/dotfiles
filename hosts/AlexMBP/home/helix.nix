11fa3cb92c9a741186955bcac8e69b0bcbefd4c5
birkhoff <git@birkhoff.me>
2025-07-11 07:24:51 +0200

    feat(helix): add mouse support and keybindings
    - Added mouse support option to the editor settings.
    - Defined statusline layout for better information display.
    - Configured keybindings for normal, insert, and select modes to include save and navigation commands.
    
    Signed-off-by: birkhoff <git@birkhoff.me>

--- hosts/AlexMBP/home/helix.nix
+++ hosts/AlexMBP/home/helix.nix
@@ -6,13 +6,31 @@
     theme = "snazzy"
 
     [editor]
+    mouse = false
     auto-save = true
     cursorline = true
     line-number = "relative"
 
+    [editor.statusline]
+    left = ["mode", "spinner", "file-absolute-path", "read-only-indicator", "file-modification-indicator"]
+    right = ["version-control", "diagnostics", "selections", "register", "position", "position-percentage", "file-encoding"]
+
     [keys.normal]
+    Cmd-s = ":write"
+    Cmd-b = "page_up"
+    Cmd-f = "page_down"
     C-j = "half_page_down"
     C-k = "half_page_up"
+
+    [keys.insert]
+    Cmd-s = ":write"
+    Cmd-b = "page_up"
+    Cmd-f = "page_down"
+    
+    [keys.select]
+    Cmd-s = ":write"
+    Cmd-b = "page_up"
+    Cmd-f = "page_down"
    '';
   };
 }
