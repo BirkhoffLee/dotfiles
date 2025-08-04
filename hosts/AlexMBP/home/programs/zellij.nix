9fe5e374a27a4b1e3c264e6aa4e185b5f58f1763
birkhoff <git@birkhoff.me>
2025-08-04 17:39:21 +0200

    feat(home): add zellij and its Unlock-First (non-colliding) preset
    Signed-off-by: birkhoff <git@birkhoff.me>

---
+++ hosts/AlexMBP/home/programs/zellij.nix
@@ -0,0 +1,18 @@
+{
+  programs.zellij = {
+    enable = true;
+
+    # Shell Integration
+    enableBashIntegration = false;
+    enableZshIntegration = true;
+
+    # Ensure we're always in a Zellij session when using
+    # the shell.
+    attachExistingSession = true;
+    exitShellOnExit = true;
+
+    # Layouts
+    # @see https://zellij.dev/documentation/layouts.html
+    layouts = {};
+  };
+}
