f88a00c4c9a51d61411ab0fd2b51a2ad8a7f0984
birkhoff <git@birkhoff.me>
2025-11-23 18:49:33 +0100

    fix public keys
    Signed-off-by: birkhoff <git@birkhoff.me>

--- hosts/shared-nix-settings.nix
+++ hosts/shared-nix-settings.nix
@@ -4,16 +4,14 @@
   nix.settings = {
     # Common substituters shared across all hosts
     substituters = [
-      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://birkhoff.cachix.org"
     ];
 
     # Common trusted public keys
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "birkhoff.cachix.org-1:m7WmdU7PKc6fsKedC278lhLtiqjz6ZUJ6v2nVGyJjQ="
+      "birkhoff.cachix.org-1:m7WmdU7PKc6fsKedC278lhLtiqjz6ZUJ6v2nkVGyJjQ="
     ];
 
     # Trusted users can use extra-substituters from flakes without warnings
