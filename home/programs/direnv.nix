784a33afa7925b25c55ba581c951e8a8e08af166
birkhoff <git@birkhoff.me>
2025-11-24 11:18:18 +0100

    chore: add explanation for nix-direnv
    Signed-off-by: birkhoff <git@birkhoff.me>

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -1,6 +1,12 @@
 {
   programs.direnv = {
     enable = true;
+
+    # Use nix-direnv to replace the built-in support
+    # for Nix. This handles caching better and prevents
+    # GC to erase our devshell caches.
+    #
+    # @see https://github.com/direnv/direnv/wiki/Nix
     nix-direnv.enable = true;
   };
 }
