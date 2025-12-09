cfd297608b3b768b56bb9b9a039fc6b77ad092eb
birkhoff <git@birkhoff.me>
2025-12-09 21:57:23 +0100

    zsh(function): add burnsrt
    Signed-off-by: birkhoff <git@birkhoff.me>

--- home/files/shell/functions.zsh
+++ home/files/shell/functions.zsh
@@ -2,6 +2,19 @@
 
 # `functions.zsh` provides helper functions and utilities.
 
+function burnsrt {
+  if [[ $# -ne 3 || ! -f "$1" || "${1##*.}" != "mp4" || ! -f "$2" || "${2##*.}" != "srt" || "${3##*.}" != "mp4" ]]; then
+    echo "Usage: burnsrt <input.mp4> <input.srt> <output.mp4>" >&2
+    echo "  - <input.mp4>: Path to a valid MP4 video file." >&2
+    echo "  - <input.srt>: Path to a valid SRT subtitle file." >&2
+    echo "  - <output.mp4>: Path for the output MP4 file." >&2
+    return 1
+  fi
+
+  ffmpeg -i "$1" -f srt -i "$2" -map 0:0 -map 0:1 -map 1:0 -c:v copy \
+    -c:a copy -c:s mov_text "$3"
+}
+
 function shrinkvid {
   if [ $# -lt 2 ]; then
     echo "Usage: shrinkvid <input_file> <output_file> [bitrate]"
