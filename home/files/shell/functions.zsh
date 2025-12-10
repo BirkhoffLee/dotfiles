e71f9787c2a0ea0151f209c7e0e24990c84206da
birkhoff <git@birkhoff.me>
2025-12-10 05:33:49 +0100

    functions: remove smite
    Signed-off-by: birkhoff <git@birkhoff.me>

--- home/files/shell/functions.zsh
+++ home/files/shell/functions.zsh
@@ -162,30 +162,6 @@ function e {
   fi
 }
 
-# Remove history entries with fzf
-# Note: doesn't work with atuin
-function smite() {
-  # https://esham.io/2025/05/shell-history
-  setopt LOCAL_OPTIONS ERR_RETURN PIPE_FAIL
-
-  local opts=( -I )
-  if [[ $1 == '-a' ]]; then
-    opts=()
-  elif [[ -n $1 ]]; then
-    print >&2 'usage: smite [-a]'
-    return 1
-  fi
-
-  fc -l -n $opts 1 | \
-    fzf --no-sort --tac --multi | \
-    while IFS='' read -r command_to_delete; do
-      printf 'Removing history entry "%s"\n' $command_to_delete
-      local HISTORY_IGNORE="${(b)command_to_delete}"
-      fc -W
-      fc -p $HISTFILE $HISTSIZE $SAVEHIST
-    done
-}
-
 # Decrypt all PDFs in the current directory
 # Example: decrypt_pdfs "password"
 function decrypt_pdfs {
