918e7c6a33f9ae6ff957e717c6abb6e80f37b951
birkhoff <git@birkhoff.me>
2026-08-10 03:04:16 +0200

    Add claude-code cache
    Signed-off-by: birkhoff <git@birkhoff.me>

--- hosts/shared-nix-settings.nix
+++ hosts/shared-nix-settings.nix
@@ -7,12 +7,14 @@
       "https://nix-community.cachix.org"
       "https://birkhoff.cachix.org"
       "https://helix.cachix.org"
+      "https://claude-code.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "birkhoff.cachix.org-1:m7WmdU7PKc6fsKedC278lhLtiqjz6ZUJ6v2nkVGyJjQ="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
     ];
 
     # Trusted users can use extra-substituters from flakes without warnings
