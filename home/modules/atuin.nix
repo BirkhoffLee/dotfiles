f541bb98d9fd473a857322bb34677bb1dc0eb929
birkhoff <git@birkhoff.me>
2026-08-26 11:30:57 +0800

    Update atuin address
    Signed-off-by: birkhoff <git@birkhoff.me>

--- home/modules/atuin.nix
+++ home/modules/atuin.nix
@@ -33,7 +33,7 @@
 
       auto_sync = true;
       sync_frequency = "5m";
-      sync_address = "http://atuin"; # Tailscale Services
+      sync_address = "http://atuin.hippo-hexatonic.ts.net";
 
       theme.name = "catppuccin-macchiato-blue";
     };
